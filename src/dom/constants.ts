915224026c3b38bef8d53f063f0e206da2fa80cf
Timur <toombez.timurtokaev@gmail.com>
2024-06-09 01:51:40 +0300

    Move constants to subdirectory

--- src/dom/constants.ts
+++ src/dom/constants.ts
@@ -11,6 +11,8 @@ export const TERMUE_ELEMENTS_NAMES = [
 
 export const TERMUE_PREFIX_NAME_SEPARATOR = ':' as const
 
-export const TERMUE_NODE_PREFIX = `node${TERMUE_PREFIX_NAME_SEPARATOR}#` as const
+export const TERMUE_NODE_PREFIX =
+    `node${TERMUE_PREFIX_NAME_SEPARATOR}#` as const
 
-export const TERMUE_ELEMENTS_PREFIX = `element${TERMUE_PREFIX_NAME_SEPARATOR}` as const
+export const TERMUE_ELEMENTS_PREFIX =
+    `element${TERMUE_PREFIX_NAME_SEPARATOR}` as const
