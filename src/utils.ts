2244dea8b7c59e377f729593a629a0bf44e04a30
Timur <toombez.timurtokaev@gmail.com>
2024-06-18 18:40:04 +0300

    Add utility to convert string to constant

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,18 @@
 import { CamelCaseToSnakeCase } from "./utilityTypes"
 
-export function camelCaseToSnakeCase<T extends string>(
-    string: T
-): CamelCaseToSnakeCase<T> {
+export function camelCaseToSnakeCase<S extends string>(
+    string: S
+): CamelCaseToSnakeCase<S> {
     const result = string
         .replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
 
-    return result as CamelCaseToSnakeCase<T>
+    return result as CamelCaseToSnakeCase<S>
+}
+
+export function convertToConstant<S extends string>(
+    string: S
+): Uppercase<CamelCaseToSnakeCase<S>> {
+    const result = camelCaseToSnakeCase(string)
+
+    return result.toUpperCase() as Uppercase<CamelCaseToSnakeCase<S>>
 }
