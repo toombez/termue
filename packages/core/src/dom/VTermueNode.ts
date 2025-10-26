405cb3e8077232dc64bd4e29d7a49cd0a9916c7e
Timur <toombez.timurtokaev@gmail.com>
2025-10-26 13:29:50 +0300

    Make Element and Node not abstract

--- packages/core/src/dom/VTermueNode.ts
+++ packages/core/src/dom/VTermueNode.ts
@@ -1,11 +1,11 @@
 import { DOM_TYPE, type DomType } from "./constants"
 import type VTermueElement from "./VTermueElement"
 
-export default abstract class VTermueNode {
+export default class VTermueNode {
     public readonly uuid: string = crypto.randomUUID()
     public _parent: VTermueElement | null = null
 
-    public readonly type: DomType = DOM_TYPE.NODE
+    protected readonly _type: DomType = DOM_TYPE.NODE
 
     public set parent(parent: VTermueElement | null) {
         this._parent?.removeChild(this)
@@ -14,5 +14,9 @@ export default abstract class VTermueNode {
 
     public get parent(): VTermueElement | null {
         return this._parent
+    }
+
+    public get type(): VTermueNode['_type'] {
+        return this._type
     }
 }
