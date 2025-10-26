d55f7fb5e7728460cbf368d2e27da2f37b4e2152
Timur <toombez.timurtokaev@gmail.com>
2025-10-26 13:03:50 +0300

    Export all from dom submodule

---
+++ packages/core/src/dom/index.ts
@@ -0,0 +1,15 @@
+import VTermueNode from './VTermueNode'
+import VTermueElement from './VTermueElement'
+import VTermueTextNode from './VTermueTextNode'
+import VTermueCommentNode from './VTermueCommentNode'
+import VTermueBoxElement from './VTermueBoxElement'
+
+export {
+    VTermueElement,
+    VTermueNode,
+    VTermueTextNode,
+    VTermueCommentNode,
+    VTermueBoxElement,
+}
+
+export * from './constants'
